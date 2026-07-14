272018bb1e0bca9d5bf1f0f1630080d62bb32e38
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-14 22:31:26 +0530

    View teams added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddTeam from './comonents/AddTeam'
+import ViewTeams from './comonents/ViewTeams'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
         <AddTeam/>
+        <ViewTeams/>
     </>
   )
 }
